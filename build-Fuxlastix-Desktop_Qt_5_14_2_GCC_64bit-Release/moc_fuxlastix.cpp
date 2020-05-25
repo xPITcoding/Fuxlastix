6df59d0fd88ea657ccbe9df53a9de4884fd9c1c1
JoAlbers <64523393+JoAlbers@users.noreply.github.com>
2020-05-25 13:34:39 +0200

    Add files via upload

---
+++ build-Fuxlastix-Desktop_Qt_5_14_2_GCC_64bit-Release/moc_fuxlastix.cpp
@@ -0,0 +1,255 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'fuxlastix.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../Fuxlastix/fuxlastix.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'fuxlastix.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_fuxlastix_t {
+    QByteArrayData data[30];
+    char stringdata0[490];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_fuxlastix_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_fuxlastix_t qt_meta_stringdata_fuxlastix = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "fuxlastix"
+QT_MOC_LITERAL(1, 10, 3), // "MSG"
+QT_MOC_LITERAL(2, 14, 0), // ""
+QT_MOC_LITERAL(3, 15, 31), // "on_pickFixedImageButton_clicked"
+QT_MOC_LITERAL(4, 47, 32), // "on_pickMovingImageButton_clicked"
+QT_MOC_LITERAL(5, 80, 33), // "on_pickMovingColourButton_cli..."
+QT_MOC_LITERAL(6, 114, 30), // "on_pickDirectoryButton_clicked"
+QT_MOC_LITERAL(7, 145, 34), // "on_pickParameterFileButton_cl..."
+QT_MOC_LITERAL(8, 180, 25), // "on_pushQuitButton_clicked"
+QT_MOC_LITERAL(9, 206, 24), // "on_pushRunButton_clicked"
+QT_MOC_LITERAL(10, 231, 10), // "outputSlot"
+QT_MOC_LITERAL(11, 242, 10), // "fertigSlot"
+QT_MOC_LITERAL(12, 253, 14), // "ganzfertigSlot"
+QT_MOC_LITERAL(13, 268, 13), // "loadTIFFImage"
+QT_MOC_LITERAL(14, 282, 5), // "fname"
+QT_MOC_LITERAL(15, 288, 5), // "scale"
+QT_MOC_LITERAL(16, 294, 5), // "split"
+QT_MOC_LITERAL(17, 300, 13), // "QList<QImage>"
+QT_MOC_LITERAL(18, 314, 7), // "saveMHD"
+QT_MOC_LITERAL(19, 322, 9), // "change_nr"
+QT_MOC_LITERAL(20, 332, 15), // "run_transformix"
+QT_MOC_LITERAL(21, 348, 5), // "merge"
+QT_MOC_LITERAL(22, 354, 8), // "QString&"
+QT_MOC_LITERAL(23, 363, 13), // "saveMHDscaled"
+QT_MOC_LITERAL(24, 377, 10), // "saveMHDall"
+QT_MOC_LITERAL(25, 388, 11), // "copy_remove"
+QT_MOC_LITERAL(26, 400, 9), // "makeCheck"
+QT_MOC_LITERAL(27, 410, 25), // "on_pickTempButton_clicked"
+QT_MOC_LITERAL(28, 436, 24), // "on_checkerButton_clicked"
+QT_MOC_LITERAL(29, 461, 28) // "on_saveCheckerButton_clicked"
+
+    },
+    "fuxlastix\0MSG\0\0on_pickFixedImageButton_clicked\0"
+    "on_pickMovingImageButton_clicked\0"
+    "on_pickMovingColourButton_clicked\0"
+    "on_pickDirectoryButton_clicked\0"
+    "on_pickParameterFileButton_clicked\0"
+    "on_pushQuitButton_clicked\0"
+    "on_pushRunButton_clicked\0outputSlot\0"
+    "fertigSlot\0ganzfertigSlot\0loadTIFFImage\0"
+    "fname\0scale\0split\0QList<QImage>\0saveMHD\0"
+    "change_nr\0run_transformix\0merge\0"
+    "QString&\0saveMHDscaled\0saveMHDall\0"
+    "copy_remove\0makeCheck\0on_pickTempButton_clicked\0"
+    "on_checkerButton_clicked\0"
+    "on_saveCheckerButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_fuxlastix[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      25,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  139,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,  142,    2, 0x08 /* Private */,
+       4,    0,  143,    2, 0x08 /* Private */,
+       5,    0,  144,    2, 0x08 /* Private */,
+       6,    0,  145,    2, 0x08 /* Private */,
+       7,    0,  146,    2, 0x08 /* Private */,
+       8,    0,  147,    2, 0x08 /* Private */,
+       9,    0,  148,    2, 0x08 /* Private */,
+      10,    0,  149,    2, 0x08 /* Private */,
+      11,    0,  150,    2, 0x08 /* Private */,
+      12,    0,  151,    2, 0x08 /* Private */,
+      13,    1,  152,    2, 0x08 /* Private */,
+      15,    1,  155,    2, 0x08 /* Private */,
+      16,    1,  158,    2, 0x08 /* Private */,
+      18,    2,  161,    2, 0x08 /* Private */,
+      19,    1,  166,    2, 0x08 /* Private */,
+      20,    1,  169,    2, 0x08 /* Private */,
+      21,    1,  172,    2, 0x08 /* Private */,
+      23,    2,  175,    2, 0x08 /* Private */,
+      24,    2,  180,    2, 0x08 /* Private */,
+      25,    0,  185,    2, 0x08 /* Private */,
+      26,    1,  186,    2, 0x08 /* Private */,
+      27,    0,  189,    2, 0x08 /* Private */,
+      28,    0,  190,    2, 0x08 /* Private */,
+      29,    0,  191,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    2,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::QImage, QMetaType::QString,   14,
+    QMetaType::QImage, QMetaType::QImage,    2,
+    0x80000000 | 17, QMetaType::QImage,    2,
+    QMetaType::Void, QMetaType::QImage, QMetaType::QString,    2,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::QImage, 0x80000000 | 22,    2,
+    QMetaType::Void, QMetaType::QImage, QMetaType::QString,    2,    2,
+    QMetaType::Void, QMetaType::QImage, QMetaType::QString,    2,    2,
+    QMetaType::Void,
+    QMetaType::QImage, QMetaType::QImage,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void fuxlastix::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<fuxlastix *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->MSG((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->on_pickFixedImageButton_clicked(); break;
+        case 2: _t->on_pickMovingImageButton_clicked(); break;
+        case 3: _t->on_pickMovingColourButton_clicked(); break;
+        case 4: _t->on_pickDirectoryButton_clicked(); break;
+        case 5: _t->on_pickParameterFileButton_clicked(); break;
+        case 6: _t->on_pushQuitButton_clicked(); break;
+        case 7: _t->on_pushRunButton_clicked(); break;
+        case 8: _t->outputSlot(); break;
+        case 9: _t->fertigSlot(); break;
+        case 10: _t->ganzfertigSlot(); break;
+        case 11: { QImage _r = _t->loadTIFFImage((*reinterpret_cast< const QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QImage*>(_a[0]) = std::move(_r); }  break;
+        case 12: { QImage _r = _t->scale((*reinterpret_cast< QImage(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QImage*>(_a[0]) = std::move(_r); }  break;
+        case 13: { QList<QImage> _r = _t->split((*reinterpret_cast< QImage(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QList<QImage>*>(_a[0]) = std::move(_r); }  break;
+        case 14: _t->saveMHD((*reinterpret_cast< const QImage(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 15: _t->change_nr((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 16: _t->run_transformix((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 17: { QImage _r = _t->merge((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QImage*>(_a[0]) = std::move(_r); }  break;
+        case 18: _t->saveMHDscaled((*reinterpret_cast< const QImage(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 19: _t->saveMHDall((*reinterpret_cast< const QImage(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 20: _t->copy_remove(); break;
+        case 21: { QImage _r = _t->makeCheck((*reinterpret_cast< QImage(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QImage*>(_a[0]) = std::move(_r); }  break;
+        case 22: _t->on_pickTempButton_clicked(); break;
+        case 23: _t->on_checkerButton_clicked(); break;
+        case 24: _t->on_saveCheckerButton_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (fuxlastix::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&fuxlastix::MSG)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject fuxlastix::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_fuxlastix.data,
+    qt_meta_data_fuxlastix,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *fuxlastix::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *fuxlastix::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_fuxlastix.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int fuxlastix::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 25)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 25;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 25)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 25;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void fuxlastix::MSG(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
